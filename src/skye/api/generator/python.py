581a3a3633c45db227ef2bb7197a1209f928fc40
Niklas Rosenstein <rosensteinniklas@gmail.com>
2022-01-07 14:32:02 +0100

    python generator: support TypeConfig.extends, add --installable, --version and --description config, support forward reference where necessary

--- src/skye/api/generator/python.py
+++ src/skye/api/generator/python.py
@@ -2,12 +2,15 @@
 import abc
 import argparse
 import builtins
+import contextlib
 import dataclasses
 import re
+import sys
 import textwrap
 import typing as t
 from pathlib import Path
 
+from .. import __version__
 from ..model import AuthenticationConfig, EndpointConfig, ErrorConfig, ModuleConfig, Project, TypeConfig
 
 
@@ -17,9 +20,9 @@ def _format_docstrings(level: int, indent: str, docs: str, width: int = 119) -> 
   lines = list(textwrap.wrap(docs, width))
   if len(lines) == 1 and len(lines[0]) < (width - 2):
     line = lines[0].replace(r"\"", r"\\\"")
-    return f'{prefix}" {line} "'
+    return f'{prefix}" {line} "\n'
   else:
-    return '\n'.join([f'{prefix}"""'] + [prefix + l for l in lines] + [f'{prefix}"""'])
+    return '\n'.join([f'{prefix}"""'] + [prefix + l for l in lines] + [f'{prefix}"""']) + '\n'
 
 
 class _Rendererable(abc.ABC):
@@ -147,7 +150,23 @@ class _PythonClass(_Rendererable):
 
 
 @dataclasses.dataclass
-class PythonGenerator:
+class _FileWriter:
+
+  stdout: bool = False
+
+  @contextlib.contextmanager
+  def open(self, filename: Path | str) -> t.Generator[t.TextIO, None, None]:
+    if self.stdout:
+      print('#', filename)
+      yield sys.stdout
+    else:
+      print('Write', filename)
+      with Path(filename).open('w') as fp:
+        yield fp
+
+
+@dataclasses.dataclass
+class CodeGenerator:
   """ A class to render Python code from a Skye-API definition. """
 
   prefix: Path | str
@@ -170,23 +189,24 @@ class PythonGenerator:
     'optional': '? | None',
   }
 
-  BUILTIN_NAMES = dir(builtins) + ['request', 'auth']
+  PYTHON_KEYWORDS = ['from', 'import', 'as', 'with', 'for', 'in', 'while', 'try', 'except', 'finally']
+  BUILTIN_NAMES = PYTHON_KEYWORDS + dir(builtins) + ['request', 'auth']
+
+  _current_module_name: str = t.cast(t.Any, None)
+  _curernt_module: ModuleConfig = t.cast(t.Any, None)
+  _current_types_already_rendered: set[str] = t.cast(t.Any, None)
 
   def write(self, stdout: bool = False, indent: str = '  ') -> None:
     """ Writes the contents of one or more modules into a Python module with the specified name. """
 
-    import sys
-    for name, value in self.modules.items():
-      filename = Path(self.prefix) / (name.replace('.', '/') + '.py')
-      self._current = name  # TODO (@nrosenstein): Dirty hack to check the current module in get_field_type()
-      module = self._build_python_module(value)
-      if stdout:
-        print('#', filename)
-        module.render(0, indent, sys.stdout)
-      else:
-        print('Write', filename)
-        with filename.open('w') as fp:
-          module.render(0, '  ', fp)
+    writer = _FileWriter(stdout)
+    for name, module in self.modules.items():
+      self._current_module_name = name
+      self._current_module = module
+      self._current_types_already_rendered = set()
+      module = self._build_python_module(module)
+      with writer.open(Path(self.prefix) / (name.replace('.', '/') + '.py')) as fp:
+        module.render(0, indent, fp)
 
   def _build_python_module(self, modules: list[ModuleConfig]) -> _PythonModule:
     python_module = _PythonModule(coding='utf-8')
@@ -225,7 +245,15 @@ class PythonGenerator:
     module.members.append(class_)
 
   def add_type(self, name: str, type_: TypeConfig, module: _PythonModule) -> None:
-    module.members.append(self._make_python_class(name, type_, module))
+    type_.validate()
+    class_ = self._make_python_class(name, type_, module)
+    if type_.values:
+      module.module_imports.add('enum')
+      for value in type_.values:
+        class_.fields.append(_PythonClassField(value.name, None, 'enum.auto()', value.docs))
+      pass
+    self._current_types_already_rendered.add(name)
+    module.members.append(class_)
 
   def get_error_base_type(self, error_code: str, module: _PythonModule) -> str:
     if error_code == 'NOT_FOUND':
@@ -240,31 +268,63 @@ class PythonGenerator:
     else:
       raise ValueError(f'unknown error_code: {error_code}')
 
-  def get_field_type(self, type_: str, module: _PythonModule) -> str:
-    match = re.match(r'(\w+)(?:\[([^\]]+)\])?', type_)
+  def get_field_type(
+    self,
+    type_: str,
+    module: _PythonModule,
+    collect_custom_types_to: list[tuple[str, str]] | None = None,
+    add_imports: bool = True,
+  ) -> str:
+    """
+    Gets the Python type name for the given type name in the YAML configuration.
+
+    :param collect_custom_types_to: Custom types found in the *type_* are added to this list if given.
+    :param add_imports: Add imports to the *module* (default: true).
+    """
+
+    match = re.match(r'(\w+)(?:\[(.+)\])?', type_)
     if not match:
       raise ValueError(f'what\'s dis? {type_!r}')
+
     type_, parameters = match.groups()
+
     if python_type := self.BUILTIN_TYPES.get(type_):
       if parameters:
         raise ValueError(f'type {type_} cannot be parametrized')
-      if '.'in python_type:
+      if '.'in python_type and add_imports:
         module.module_imports.add(python_type.rpartition('.')[0])
       return python_type
+
     if python_type := self.PARAMETRIZED_TYPES.get(type_):
-      split_parameters = [x.strip() for x in parameters.split(',')]
+      split_parameters = [x.strip() for x in parameters.split(',')] if parameters else []
       num_params = python_type.count('?')
       if len(split_parameters) != num_params:
         raise ValueError(f'type {type_} requires {num_params} parameters, got {len(split_parameters)}')
-      split_parameters = [self.get_field_type(x, module) for x in split_parameters]
+
+      custom_types_in_parameters = []
+      split_parameters = [self.get_field_type(x, module, custom_types_in_parameters) for x in split_parameters]
+
+      # Special handling for the optional type, if we need to use a forward reference then we cannot
+      # use the | syntax but need to use the typing module.
+      if type_ == 'optional' and any(t[1] not in self._current_types_already_rendered for t in custom_types_in_parameters):
+        assert len(split_parameters) == 1
+        module.module_imports.add('typing')
+        return f'typing.Optional[{split_parameters[0]}]'
+
       return python_type.replace('?', '{}').format(*split_parameters)
+
     # Try to resolve the type in the available modules.
     for module_name, modules in self.modules.items():
       for module_cfg in modules:
         if type_ in module_cfg.types:
-          if self._current != module_name:
+          available = (type_ in self._current_types_already_rendered)
+          if self._current_module_name != module_name and add_imports:
             module.member_imports.add(module_name + '.' + type_)
-          return type_
+            available = True
+          if collect_custom_types_to is not None:
+            collect_custom_types_to.append((module_name, type_))
+          return repr(type_) if not available else type_
+
     raise ValueError(f'unknown type: {type_}')
 
   def get_auth_decorators(self, auth: AuthenticationConfig | None, module: _PythonModule) -> list[str]:
@@ -312,6 +372,49 @@ class PythonGenerator:
     )
 
 
+@dataclasses.dataclass
+class ProjectGenerator:
+
+  prefix: Path | str
+  codegen: CodeGenerator
+  module: str | None = None
+  package: str | None = None
+  version: str | None = None
+  description: str | None = None
+
+  def write(self, stdout: bool = False) -> None:
+    assert self.package or self.module
+
+    writer = _FileWriter(stdout)
+
+    if self.package:
+      with writer.open(self.prefix / 'src' / self.package.replace('.', '/') / '__init__.py') as fp:
+        for module_name in self.codegen.modules:
+          fp.write(f'from {module_name} import *\n')
+
+    if not self.version:
+      self.version = '0.0.0'
+    if not self.description:
+      self.description = 'Auto-generated API bindings.'
+
+    with writer.open(self.prefix / 'pyproject.toml') as fp:
+      fp.write('\n'.join([
+        '[build-system]',
+        'requires = ["flit_core >=3.2,<4"]',
+        'build-backend = "flit_core.buildapi"',
+        '',
+        '[project]',
+        f'name = {self.module or self.package!r}',
+        f'version = {self.version!r}',
+        'authors = []',
+        f'description = {self.description!r}',
+        'dependencies = [',
+        f'  "skye-api ~={__version__}",',
+        ']',
+        '',
+      ]))
+
+
 def get_argument_parser() -> argparse.ArgumentParser:
   parser = argparse.ArgumentParser()
   parser.add_argument(
@@ -323,8 +426,24 @@ def get_argument_parser() -> argparse.ArgumentParser:
   parser.add_argument(
     '--prefix',
     metavar='PATH',
-    help='Generate files starting from the specified directory (defaults to cwd).',
-    required=True,
+    help='Generate files starting from the specified directory.',
+    type=Path,
+  )
+  parser.add_argument(
+    '--installable',
+    metavar='PATH',
+    help='Generate a fully installable Pyhon project at the specified location. Cannot be mixed with --prefix',
+    type=Path,
+  )
+  parser.add_argument(
+    '--version',
+    metavar='VERSION',
+    help='The version number to write into the generated project (only with --installable).',
+  )
+  parser.add_argument(
+    '--description',
+    metavar='TEXT',
+    help='The description to write into the generated project (only with --installable).',
   )
   parser.add_argument(
     '--stdout',
@@ -358,23 +477,40 @@ def main():
   parser = get_argument_parser()
   args = parser.parse_args()
 
+  if args.installable and args.prefix:
+    parser.error('--installable and --prefix cannot be mixed')
+  if not args.installable and not args.prefix:
+    parser.error('one of --installable or --prefix must be specified')
+
   if args.module and args.package:
     parser.error('--module and --package cannot be mixed')
   if not args.module and not args.package:
     parser.error('one of --module or --package must be specified')
 
+  if args.version and not args.installable:
+    parser.error('--version can only be used with --installable')
+  if args.description and not args.installable:
+    parser.error('--description can only be used with --installable')
+
+  # Load the configuration files into a project.
   project = Project()
   for filename in map(Path, args.files):
     project.add(filename.stem, filename)
 
-  generator = PythonGenerator(args.prefix)
+  # Configure the code generator.
+  if args.installable:
+    args.prefix = args.installable / 'src'
 
+  codegen = CodeGenerator(args.prefix)
   if args.module:
-    generator.modules = {args.module: list(project.items.values())}
+    codegen.modules = {args.module: list(project.items.values())}
   else:
-    generator.modules = {args.package + '.' + k: [m] for k, m in project.modules.items()}
+    codegen.modules = {args.package + '.' + k: [m] for k, m in project.modules.items()}
+  codegen.write(stdout=args.stdout)
 
-  generator.write(stdout=args.stdout)
+  if args.installable:
+    projectgen = ProjectGenerator(args.installable, codegen, args.module, args.package, args.version, args.description)
+    projectgen.write(args.stdout)
 
 
 if __name__ == '__main__':
--- src/skye/api/model/_type.py
+++ src/skye/api/model/_type.py
@@ -1,5 +1,6 @@
 
 import dataclasses
+import itertools
 
 
 @dataclasses.dataclass
@@ -20,6 +21,9 @@ class TypeConfig:
   #: If set, the type defines an enumeration of the specified values.
   values: list[ValueConfig] | None = None
 
+  #: The name of a type that this type extends.
+  extends: str | None = None
+
   #: If set, the type defines a structure of the specified values.
   fields: dict[str, FieldConfig] | None = None
 
@@ -33,3 +37,9 @@ class TypeConfig:
   ref: str | None = None
 
   docs: str | None = None
+
+  def validate(self) -> None:
+    groups = [('values',), ('extends', 'fields'), ('union',), ('ref',)]
+    for group1, group2 in itertools.permutations(groups, 2):
+      if any(getattr(self, n) is not None for n in group1) and any(getattr(self, n) is not None for n in group2):
+        raise ValueError(f'TypeConfig {group1} cannot be mixed with {group2}')
